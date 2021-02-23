3f0146819e176993dc0155fb2943dfe76f76e1ec
tangandkai <39239312+tangandkai@users.noreply.github.com>
2021-02-23 22:19:53 +0800

    gateway添加断言predicates

--- spring-cloud-demo/spring-cloud-gateway9527/src/main/java/com/tang/gateway/SpringCloudGateway9527Application.java
+++ spring-cloud-demo/spring-cloud-gateway9527/src/main/java/com/tang/gateway/SpringCloudGateway9527Application.java
@@ -2,8 +2,10 @@ package com.tang.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringCloudGateway9527Application {
 
 	public static void main(String[] args) {
---
+++ spring-cloud-demo/spring-cloud-gateway9527/src/main/java/com/tang/gateway/config/GatewayConfig.java
@@ -0,0 +1,29 @@
+package com.tang.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 编码自定义路由
+ */
+@Configuration
+public class GatewayConfig {
+    /**
+     * 配置相当于在yml中配置了一个id为path_route_hello的路由规则
+     * 当访问地址http://localhost:9527/guonei的时候，gateway会通过断言是否为true
+     * 匹配到对应的URI地址：http://news.baidu.com/guonei
+     *
+     * @param routeLocatorBuilder
+     * @return
+     */
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route( "path_route_hello",
+                r -> r.path( "/guonei" )
+                        .uri( "http://news.baidu.com/guonei")).build();
+        return routes.build();
+    }
+}
