77b121a36a6cb09a13836db928066d635372ae0c
tangandkai <39239312+tangandkai@users.noreply.github.com>
2021-02-25 22:56:45 +0800

    springcloud的config客户端配置

---
+++ spring-cloud-demo/spring-cloud-configclient3355/src/main/java/com/tang/config/SpringCloudConfigclient3355Application.java
@@ -0,0 +1,13 @@
+package com.tang.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigclient3355Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigclient3355Application.class, args);
+    }
+
+}
