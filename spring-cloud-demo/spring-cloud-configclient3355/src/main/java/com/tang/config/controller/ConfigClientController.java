fac639ac6fa1b27a7cdd753c25ab202b124255a4
tangandkai <39239312+tangandkai@users.noreply.github.com>
2021-02-25 23:08:48 +0800

    config手动刷新配置

--- spring-cloud-demo/spring-cloud-configclient3355/src/main/java/com/tang/config/controller/ConfigClientController.java
+++ spring-cloud-demo/spring-cloud-configclient3355/src/main/java/com/tang/config/controller/ConfigClientController.java
@@ -1,10 +1,12 @@
 package com.tang.config.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope //curl -X POST "http://localhost:3355/actuator/refresh" 刷新
 public class ConfigClientController {
 
     @Value("${config.info}")
